23adac16476c122e3d6dabd17fa78eecefded2cf
Annabelle Zhang <qiaoyan1@ualberta.ca>
2020-01-06 18:13:25 -0700

    modified

--- cmput404.py
+++ cmput404.py
@@ -2,6 +2,6 @@ import requests
 
 print(requests.__version__)
 
-var = requests.get("https://www.google.com/")
+var = requests.get("https://raw.githubusercontent.com/QiaoyanAnna/CMPUT404/master/cmput404.py")
 
 print(var.content)
